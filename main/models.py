7df20c1adb7cba47d83e8bf6d51124d4f9c76391
NizamovRinat31 <rinat.partnernk@gmail.com>
2024-03-13 21:10:03 +0300

    добавили товары

--- main/models.py
+++ main/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Product(models.Model):
-    title = models.CharField(max_length=255, blank=False)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    discount_price = models.DecimalField(max_digits=12, decimal_places=2, null=True)
-    count = models.IntegerField(default=0)
+    title = models.CharField(max_length=255, blank=False, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    # цена за тонну в рублях
+    price = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Цена')
+    discount_price = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True, verbose_name='Цена со скидкой')
+    count = models.IntegerField(default=0, verbose_name='Колличество')
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        
+    def __str__(self):
+        return self.title
     
