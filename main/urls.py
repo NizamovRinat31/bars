c9e9374485f7f758301f874193c901835b0ef6f2
NizamovRinat31 <rinat.partnernk@gmail.com>
2024-03-20 21:26:24 +0300

    basket

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index_view, name='index'),
+    path('', views.products_view, name='products'),
+    path('product/<int:id>/', views.product_view, name='product'),
+    path(
+        'product/<int:id>/add/',
+        views.add_to_basket_view,
+        name='add_to_basket'
+    ),
+    path('basket/', views.basket_view, name='basket'),
 ]
